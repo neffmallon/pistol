afffc0202d98bf4f0b7b5eb8d1290473114f1491
rpmuller <rpmuller@fa78dccd-b01a-0410-b0aa-a9cb8a393f34>
2008-02-02 21:56:56 +0000

    Solved 123

--- ProjectEuler/P100.py
+++ ProjectEuler/P100.py
@@ -1,120 +1,27 @@
-import psyco; psyco.full()
-
 from math import sqrt
-from sets import Set
-from Utils import primes,gcd
-from pylab import *
-
-def avg(a,b): return (a+b)/2.
-def isqrt(n,maxiter=100,f=1):
-    for i in range(maxiter):
-        f = avg(f,n/f)
-    return f
-
-def isint(n): return int(n) == n
-
-def isintsq(n): return isint(sqrt(n))
-
-def isoddsquare(n,abc,tol=1e-9):
-    return abs(sqrt(f(n,abc))%2-1) < tol
-
-def f(n,(a,b,c)): return a*n*n+b*n+c
-
-    
-nmax = 1001000
-ps = primes(nmax)
-pset = Set(ps)
-
-def getfactors(n):
-   """Return list containing prime factors of a number."""
-   if isprime(n) or n==1:
-       return [n]
-   for i in ps:
-       if not n%i: # if goes evenly
-           n = n/i
-           return [i] + getfactors(n)
-   return []
-
-def isprime(n): return n in pset
+from itertools import count
 
 def quadratic(a,b,c):
     n1 = -b
     disc = b*b-4*a*c
     if disc < 0: return None,None
-    #n2 = sqrt(disc)
-    n2 = isqrt(disc)
+    n2 = sqrt(disc)
     return (n1+n2)/(2*a),(n1-n2)/(2*a)
 
-def brute():
-    t =1000000000000L
-    m = 1000000
-    for i in xrange(m):
+def isint(n): return int(n) == n
+def issqint(n): return isint(sqrt(n))
+
+def main():
+    def disc(n): return 2*n*(n-1)+1
+    t = 10**12
+    for i in count():
         nt = t+i
-        nb1,nb2 = quadratic(2,-2,-nt*(nt-1))
-        nb = int(nb1)
-        if isint(nb1):
-            print nt,nb,2*nb*(nb-1)==nt*(nt-1)
+        if issqint(disc(nt)):
+            d = int(sqrt(disc(nt)))
+            nb = (d+1)/2
+            print nt,disc(nt),nb,2*nb*(nb-1)==nt*(nt-1)
     return
 
-def brute2():
-    t =1000000000000L
-    b = 1000000000
-    m = 1000000
-    for i in xrange(2*b):
-        nt = t+i
-        denom = nt*(nt-1)
-        nb1,nb2 = quadratic(2,-2,-nt*(nt-1))
-        inb1 = long(nb1)
-        win = 1
-        for nb in range(inb1-win,inb1+win+1):
-            num = 2*nb*(nb-1)
-            if num==denom:
-                print nb,nt,num,denom
-                break
-            if num > denom: break
-        if i % m == 0: print i
-    return
+if __name__ == '__main__': main()
 
-def clever2():
-    thou = 1000
-    mill = 1000000
-    bill = 1000000000
-    trill = 1000000000000L
-    nmax = thou
-    # We want to find the solutions where 2*k^2-1 is a perfect square
-    ks = []
-    for k in range(2,mill):
-        s = sqrt(2*k*k-1)
-        i = long(s)
-        if i == s and i%2==1:
-            nt = (1 + i)/2
-            nb = (1 + sqrt(1+2*nt*(nt-1)))/2
-            print k,nt,nb,2.*nb*(nb-1)/(nt*(nt-1))
-            ks.append(i)
-    plot(ks)
-    show()
-    
-
-def clever():
-    t =1000000000000L
-    b = 1000000000
-    m = 1000000
-    for i in xrange(2*b):
-        nt = t+i
-        q = 2*nt*nt - 2*nt + 1
-        qrt = long(sqrt(q))
-        sqs = []
-        win = 1
-        #print "Seeking sqrt of ",q
-        #print "Approximate sqrt = ",qrt,qrt*qrt
-        for i in range(-win,win+1):
-            qrti = qrt + i
-            qrti2 = qrti*qrti
-            if q == qrti2:
-                "Solution found: nt = ",nt
-                "sqrt(2nt^2-2nt+1) = ",qrti
-                break
-            elif qrti2 > q:
-                break
-            #print "!= ",qrti2
-brute2()
+        
--- ProjectEuler/P119.py
+++ ProjectEuler/P119.py
@@ -4,19 +4,33 @@ from time import time
 
 def simple_powers_table(nmax=100,pmax=10):
     spt = [[],[1]]
-    for i in range(2,nmax):
+    for i in xrange(2,nmax):
         spti = [1]
         spt.append(spti)
         p = i
-        for j in range(1,pmax):
+        for j in xrange(1,pmax):
             spti.append(p)
             p*=i
     return spt
 
+def clever(nmax=100):
+    from itertools import count
+    spt = simple_powers_table(200,20)
+    index = 1
+    for i in count(10):
+        digsum = sum(map(int,str(i)))
+        if i % digsum > 0: continue
+        if i % digsum**2 > 0: continue
+        if i in spt[digsum]:
+            print "(%3d) %d == %d**%d" % (index,i,digsum,spt[digsum].index(i))
+            index += 1
+    return
+
 def brute(nmax=10000000,pmax=10):
     index = 1
-    for i in range(10,nmax):
+    for i in xrange(10,nmax):
         digsum = sum(map(int,str(i)))
+        if i % digsum > 0: continue
         for j in range(1,pmax):
             p = pow(digsum,j)
             if p == i:
@@ -25,19 +39,46 @@ def brute(nmax=10000000,pmax=10):
             if p > i: break
     return
 
-def clever(nmax=100000000):
-    spt = simple_powers_table()
-    index = 1
-    for i in xrange(10,nmax):
-        digsum = sum(map(int,str(i)))
-        if i in spt[digsum]:
-            print "(%3d) %d == %d**x" % (index,i,digsum)
-            index += 1
-    return
+def digsum(i): return sum(map(int,str(i)))
+
+def clever2(nmax=200,pmax=20):
+    results = []
+    for i in xrange(2,nmax):
+        p = i
+        for j in xrange(1,pmax):
+            if p>10 and digsum(p) == i:
+                results.append(p)
+            p*=i
+    results.sort()
+    for i,p in enumerate(results[:40]):
+        print i+1,p
+    return 
+
+
+# Here's what I've found so far:
+# (  1) 81 == 9**2
+# (  2) 512 == 8**3
+# (  3) 2401 == 7**4
+# (  4) 4913 == 17**3
+# (  5) 5832 == 18**3
+# (  6) 17576 == 26**3
+# (  7) 19683 == 27**3
+# (  8) 234256 == 22**4
+# (  9) 390625 == 25**4
+# ( 10) 614656 == 28**4
+# ( 11) 1679616 == 36**4
+# ( 12) 17210368 == 28**5
+# ( 13) 34012224 == 18**6
+# ( 14) 52521875 == 35**5
+# ( 15) 60466176 == 36**5
+# ( 16) 205962976 == 46**5
+# ( 17) 612,220,032 == 18**7
 
 def main():
     t0 = time()
-    clever()
+    nmax=1000000000
+    #brute(nmax)
+    clever2()
     print time()-t0
     return
 
