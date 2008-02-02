69cfad2cfa8b9e68aed689f1ecaf2e160be5b088
rpmuller <rpmuller@fa78dccd-b01a-0410-b0aa-a9cb8a393f34>
2008-02-02 22:19:01 +0000

    Working on P100

--- ProjectEuler/P100.py
+++ ProjectEuler/P100.py
@@ -12,14 +12,14 @@ def isint(n): return int(n) == n
 def issqint(n): return isint(sqrt(n))
 
 def main():
-    def disc(n): return 2*n*(n-1)+1
-    t = 10**12
+    #def disc(n): return 2*n*(n-1)+1
+    rt2 = sqrt(2)
+    trillion = 10**12
     for i in count():
-        nt = t+i
-        if issqint(disc(nt)):
-            d = int(sqrt(disc(nt)))
-            nb = (d+1)/2
-            print nt,disc(nt),nb,2*nb*(nb-1)==nt*(nt-1)
+        nt = i+trillion
+        nb = int(round(nt/rt2))
+        if 2*nb*(nb-1) == nt*(nt-1):
+            print nt,nb
     return
 
 if __name__ == '__main__': main()
