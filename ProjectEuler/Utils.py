4696cbb8fe2e32e32e4981d873ae9e173fb1564b
rpmuller <rpmuller@fa78dccd-b01a-0410-b0aa-a9cb8a393f34>
2008-12-08 02:32:37 +0000

    Solved 207

--- ProjectEuler/Utils.py
+++ ProjectEuler/Utils.py
@@ -49,6 +49,18 @@ def proper_divisors(n):
     d.sort()
     return d
 
+def ndivisors(n,VERBOSE=False):
+    "just count the divisors of n"
+    s = 0
+    lim = int(sqrt(n)+1)
+    for i in xrange(1,lim):
+        if n%i == 0:
+            if i == n/i:
+                s += 1
+            else:
+                s+=2
+    return s
+
 def divisors(n):
     "Also include n"
     return proper_divisors(n) + [n]
